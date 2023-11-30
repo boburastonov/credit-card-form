6d4cee4e4632404e27a7a277b47fa825b78f958e
boburastonov <boburastonov33@gmail.com>
2023-11-30 23:27:40 +0500

    add new function

--- script.js
+++ script.js
@@ -12,19 +12,28 @@ const cvvBox = document.querySelector(".cvv-box");
 const cvvInput = document.querySelector(".cvv-input");
 
 cardNumberInput.addEventListener("input", () => {
+  if (cardNumberInput.value.length > 16) {
+    cardNumberInput.value = cardNumberInput.value.slice(0, 16);
+  }
   cardNumberBox.innerText = cardNumberInput.value;
 });
 cardHolderInput.addEventListener("input", () => {
   cardHolderName.innerText = cardHolderInput.value;
+  if (cardHolderInput.value.length === 21) {
+    cardHolderName.innerText = cardHolderInput.value.slice(0, 12) + '...';
+  }
 });
 monthInput.addEventListener("input", () => {
   expMonth.innerText = monthInput.value;
 });
 yearInput.addEventListener("input", () => {
-      const sl = yearInput.value.slice(yearInput.value.length - 2);
-      expYear.innerText = sl;
+  const sl = yearInput.value.slice(yearInput.value.length - 2);
+  expYear.innerText = sl;
 });
 cvvInput.addEventListener("input", () => {
+  if (cvvInput.value.length > 4) {
+    cvvInput.value = cvvInput.value.slice(0, 4);
+  }
   cvvBox.innerText = cvvInput.value;
 });
 cvvInput.addEventListener("mouseenter", () => {
