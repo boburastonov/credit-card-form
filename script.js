ec95001a56726813802c174a85f3ef4fe35b1a4e
boburastonov <boburastonov33@gmail.com>
2023-11-30 21:36:04 +0500

    update all styles

--- script.js
+++ script.js
@@ -1,89 +1,37 @@
-/*
-See on github: https://github.com/muhammederdem/credit-card-form
-*/
+const front = document.querySelector(".front");
+const back = document.querySelector(".back");
+const cardNumberBox = document.querySelector(".card-number-box");
+const cardNumberInput = document.querySelector(".card-number-input");
+const cardHolderName = document.querySelector(".card-holder-name");
+const cardHolderInput = document.querySelector(".card-holder-input");
+const expMonth = document.querySelector(".exp-month");
+const monthInput = document.querySelector(".month-input");
+const expYear = document.querySelector(".exp-year");
+const yearInput = document.querySelector(".year-input");
+const cvvBox = document.querySelector(".cvv-box");
+const cvvInput = document.querySelector(".cvv-input");
 
-new Vue({
-  el: "#app",
-  data() {
-    return {
-      currentCardBackground: Math.floor(Math.random() * 25 + 1), // just for fun :D
-      cardName: "",
-      cardNumber: "",
-      cardMonth: "",
-      cardYear: "",
-      cardCvv: "",
-      minCardYear: new Date().getFullYear(),
-      amexCardMask: "#### ###### #####",
-      otherCardMask: "#### #### #### ####",
-      cardNumberTemp: "",
-      isCardFlipped: false,
-      focusElementStyle: null,
-      isInputFocused: false,
-    };
-  },
-  mounted() {
-    this.cardNumberTemp = this.otherCardMask;
-    document.getElementById("cardNumber").focus();
-  },
-  computed: {
-    getCardType() {
-      let number = this.cardNumber;
-      let re = new RegExp("^4");
-      if (number.match(re) != null) return "visa";
-
-      re = new RegExp("^(34|37)");
-      if (number.match(re) != null) return "amex";
-
-      re = new RegExp("^5[1-5]");
-      if (number.match(re) != null) return "mastercard";
-
-      re = new RegExp("^6011");
-      if (number.match(re) != null) return "discover";
-
-      re = new RegExp("^9792");
-      if (number.match(re) != null) return "troy";
-
-      return "visa"; // default type
-    },
-    generateCardNumberMask() {
-      return this.getCardType === "amex"
-        ? this.amexCardMask
-        : this.otherCardMask;
-    },
-    minCardMonth() {
-      if (this.cardYear === this.minCardYear) return new Date().getMonth() + 1;
-      return 1;
-    },
-  },
-  watch: {
-    cardYear() {
-      if (this.cardMonth < this.minCardMonth) {
-        this.cardMonth = "";
-      }
-    },
-  },
-  methods: {
-    flipCard(status) {
-      this.isCardFlipped = status;
-    },
-    focusInput(e) {
-      this.isInputFocused = true;
-      let targetRef = e.target.dataset.ref;
-      let target = this.$refs[targetRef];
-      this.focusElementStyle = {
-        width: `${target.offsetWidth}px`,
-        height: `${target.offsetHeight}px`,
-        transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`,
-      };
-    },
-    blurInput() {
-      let vm = this;
-      setTimeout(() => {
-        if (!vm.isInputFocused) {
-          vm.focusElementStyle = null;
-        }
-      }, 300);
-      vm.isInputFocused = false;
-    },
-  },
+cardNumberInput.addEventListener("input", () => {
+  cardNumberBox.innerText = cardNumberInput.value;
+});
+cardHolderInput.addEventListener("input", () => {
+  cardHolderName.innerText = cardHolderInput.value;
+});
+monthInput.addEventListener("input", () => {
+  expMonth.innerText = monthInput.value;
+});
+yearInput.addEventListener("input", () => {
+      const sl = yearInput.value.slice(yearInput.value.length - 2);
+      expYear.innerText = sl;
+});
+cvvInput.addEventListener("input", () => {
+  cvvBox.innerText = cvvInput.value;
+});
+cvvInput.addEventListener("mouseenter", () => {
+  front.style.transform = "perspective(1000px) rotateY(-180deg)";
+  back.style.transform = "perspective(1000px) rotateY(0deg)";
+});
+cvvInput.addEventListener("mouseleave", () => {
+  front.style.transform = "perspective(1000px) rotateY(0deg)";
+  back.style.transform = "perspective(1000px) rotateY(180deg)";
 });
